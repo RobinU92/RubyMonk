b62f7fec8f7220994232134ce28c5c83b2af4d5a
Andre Jones <andreij6@gmail.com>
2013-10-03 11:05:11 -0500

    Introduction to Objects Completed

--- Objects.rb
+++ Objects.rb
@@ -1,1 +1,54 @@
- 1.methods #shows all methods that can be used on an object
+=begin  Ruby Everything is an object 
+
+
+    My Ruby Learning/Review Material - Solidifying ruby basics
+
+  Andre' I. Jones 10/3/2013
+  15 mins of review
+  15 mins of coding review material
+  30 mins learning something new
+
+=end            Ruby Primer
+
+  #Introduction to objects
+  #00. 
+  
+  #Everthing is an Object
+  self          #main
+  
+  #Talking to objects
+  2.even?         #True
+  2.next.next     #4
+  
+  #01 More Objects and Methods
+  
+  #Looking up methods
+  1.methods                                           #shows all methods that can be used on an object
+  1.methods.sort                                      # + sorted
+  
+  
+  #invoking methods with arguments
+  ['rock','paper','scissors'].index('paper')          # returns the index of the given arg which is 1
+  2.between?(1,5)                                     # true  
+  
+  #0.2 Syntactic Sugar for Special Methods
+  
+  #Special Methods
+  
+  #puts 4+3               7
+  #puts 4.+(3)            same
+  
+  #        +   -   *   /   =   ==    !=    >   <   >=    <=    []
+  
+  words = ["foo",'bar', "baz"]      #creates a variable words that holds an array of strings
+  words[1]                          #gets the value at index 1 which is bar
+  words.[](1)                       #same 
+
+
+
+
+
+
+
+
+
