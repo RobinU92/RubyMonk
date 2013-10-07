8d8a08056613230088b28851b73e47206580e2de
Andre Jones <andreij6@gmail.com>
2013-10-07 15:45:20 -0500

    Control Structures

--- ControlStructures.rb
+++ ControlStructures.rb
@@ -16,14 +16,53 @@
   
 #The if..else construct
   #Ruby Conditional Branching : the 'if' statement
-  #the ternary operator
-  #Truthiness of objects in Ruby
+  def check_sign(number)
+    if number > 0
+      "#{number} is positive"
+    else
+      "#{number} is negative"
+    end        
+  end
+  
+  check_sign(0)       #returns 0 is negative
+  check_sign(1)       # returns 1 is positve
+  check_sign(-1)      # -1 is negative
+  
+  def check_signNEW(number)
+    if number > 0
+      "#{number} is positive"
+    elsif number == 0
+      "0"
+    else
+      "#{number} is negative"
+    end        
+  end
 
+
+  #the ternary operator
+  def check_sign(number)
+    number > 0 ? "#{number} is positive" : "#{number} is negative"
+  end
+  
+  #Truthiness of objects in Ruby
+  #objects false and nil equates to false. Every other object like say 1, 0, "" are all evaluated to be true.
+  if 0
+    puts "Hey, 0 is considered to be a truth in Ruby"
+  end
 
 #Loops in Ruby
-  #
+  loop do
+    monk.meditate
+    break if monk.nirvana?
+  end
 
-
+  def ring(bell, n)
+    n.times do
+      bell.ring
+    end
+  end  
+  
+  
 
 
 
