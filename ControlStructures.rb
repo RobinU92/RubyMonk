efb018f54aaaba0d3d6f9778863d21f35c189534
Andre Jones <andreij6@gmail.com>
2013-10-07 10:59:42 -0500

    CS

---
+++ ControlStructures.rb
@@ -0,0 +1,30 @@
+#BOOLEAN EXPRESSIONS IN RUBY
+
+  #Beginners guide to Expressions in Ruby
+  
+  name == "Bob"     #checks if the variable name is equal to the string Bob
+  age <= 35         #checks if the variable age is less than or equal to 35
+  
+  #Combining Expressions using && and || operators
+  
+  age >= 23 && name == 'Bob'                          #and Both conditions need to be true
+  age < 14 || name == "Craig"                         #or Only one condition needs to be true
+  age >= 23 && (name == 'Bob' || name == 'Jill')       
+  
+  #Negating expressions
+  !(name == "Bob")                                    #any value for name will evaluate to true but Bob.
+  
+#The if..else construct
+  #Ruby Conditional Branching : the 'if' statement
+  #the ternary operator
+  #Truthiness of objects in Ruby
+
+
+#Loops in Ruby
+  #
+
+
+
+
+
+
