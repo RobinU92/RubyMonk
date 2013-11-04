91e43b5e44d1d5a5cc5db6abce83c70bd145c887
Andre Jones <andreij6@gmail.com>
2013-11-03 23:01:27 -0600

    notes

---
+++ introToDatabasesLynda.rb
@@ -0,0 +1,39 @@
+=begin Introducing Databases
+
+1 application   = 1 database
+1 model         = 1 table
+
+Column: a set of data of a single simple type
+  1 attribute = 1 column
+  
+Row: a single record of data
+  1 object or instance = 1 row
+  
+Field: the intersection of a column and a row
+  Has a single value
+  
+Foreign Key: a table column whose values reference rows in another table
+  THE FOUNDATION OF RELATION DATABESES
+  
+  Ex. subject_id located in the pages table would be the foreign key
+  
+Index: a data structure on a table to increase lookup speed
+  Like the index at the back of a book
+
+Schema: the structural definition of a database
+  Defines the tables, columns, and indexes of a database
+
+=end
+
+=begin Introducing Migrations
+  
+  A set of database instructions
+  Allows you to "migrate" your database from one state to another
+    Moving up to a new state
+    Moving back "down" to an previous state
+    
+  
+=end
+  
+
+  
