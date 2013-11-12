3caa11167a52a4919e4542689b4229f15e4cbb54
Andre Jones <andreij6@gmail.com>
2013-11-12 10:29:02 -0600

    one more lesson

--- MandNamespaces.rb
+++ MandNamespaces.rb
@@ -1,3 +1,4 @@
+# Reviewed 11/4/2013
 module Perimeter
   class Array
     def initialize
@@ -12,4 +13,30 @@ ruby_array  = Array.new
 p our_array.class
 p ruby_array.class
 
-# Reviewed 11/4/2013
+
+
+class Array
+  def initialize
+    @size = 400
+  end
+end
+
+our_array = Array.new
+
+p our_array.class
+
+module Kata
+  A = 5
+  module Dojo
+    B = 9
+    A = 7
+    
+    class ScopeIn
+      def push
+        ::A
+      end
+    end
+  end
+end
+
+A = 10
